c438e0d3a06be8bf8646d036ed44a192e536d383
Risto Rushford <cbr3@pdx.edu>
2021-03-18 16:03:49 -0700

    Added heatmap to digitTest.py

--- digitTest.py
+++ digitTest.py
@@ -1,9 +1,12 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 from numpy.linalg import norm
 from op import op
+
+"""
+RPCA demo - digitTest
+"""
 
 ## Load data
 data = loadmat("mnistSubset.mat")
@@ -21,12 +24,31 @@ Y = np.append(Y, Xk[:, 100:111], axis=1)
 
 print(Y.shape)
 
-L, C = op(Y, test='mnist')
-l2norm_C = []
+lam_list = np.linspace(0.005, 0.009, 10)
+print(lam_list)
+heatmap = np.zeros((len(lam_list), len(lam_list)))
+for l in range(len(lam_list)):
+    for lc in range(len(lam_list)):
+        lam = lam_list[l]
+        lam_c = lam_list[lc]
+        L, C = op(Y, test='mnist', lam = lam, lam_s = lam_c)
+        Yhat = L + C
+        heatmap[l, lc] = norm(Y - Yhat, ord='fro') / norm(Y, ord='fro')
 
-for ii in range(Y.shape[1]):
-    column_norm = norm(C[:, ii])
-    l2norm_C.append(column_norm)
+        print(l, lc)
+
+plt.imshow(heatmap)
+plt.colorbar()
+plt.title("Heat Map")
+plt.savefig('digitTest_heatmap_1')
+plt.show()
+
+
+# l2norm_C = []
+
+# for ii in range(Y.shape[1]):
+#     column_norm = norm(C[:, ii])
+#     l2norm_C.append(column_norm)
 
 # outliers = np.argwhere(np.array(l2norm_C) > 2)
 # print(outliers)
@@ -53,7 +75,5 @@ for ii in range(Y.shape[1]):
 # plt.suptitle('Example Images')
 # plt.show()
 
-
-plt.stem(l2norm_C)
-plt.show()
-
+# plt.stem(l2norm_C)
+# plt.show()
